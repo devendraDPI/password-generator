9dae261472539ca2a6f906a655e7be9567e3e42a
Devendra <76481165+Devendra-DPI@users.noreply.github.com>
2022-11-01 13:26:40 +0530

    v4.0.0

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,19 +1,29 @@
 # Devendra-DPI
 
 import string
+import random
 import secrets
 import time
 
+def password_generator(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    pwd = ''
+    for i in range(length):
+        pwd += ''.join(secrets.choice(characters))
+    return pwd
+
+def custom_password_generator(length, custom_characters):
+    characters = custom_characters + string.ascii_letters + string.digits + string.punctuation
+    pwd = ''
+    for i in range(length - len(custom_characters)):
+        pwd += ''.join(secrets.choice(characters))
+    pwd += ''.join(custom_characters)
+    return ''.join(random.sample(pwd, len(pwd)))
+
 print("********************************\n**     PASSWORD GENERATOR     **\n********************************")
 while True:
-    user_input = input("\nGenerate (G), Quit (Q): ").casefold()
-
-    def password_generator(length):
-        characters = string.ascii_letters + string.digits + string.punctuation
-        pwd = ''
-        for i in range(length):
-            pwd += ''.join(secrets.choice(characters))
-        return pwd
+    print("\n+------------------------------+\n|   Generate (G), Custom (C)   |\n|   Quit (Q)                   |\n+------------------------------+")
+    user_input = input("Input: ").casefold()
 
     try:
         if user_input == "g":
@@ -23,9 +33,17 @@ while True:
             else:
                 print("Password must be at least 10 characters long")
                 print(f"Password (10): {password_generator(10)}")
+        elif user_input == "c":
+            custom_characters = input("Enter characters to be in password: ")
+            length = int(input("Password Length: "))
+            if length >= 10:
+                print(f"Password ({length}): {custom_password_generator(length, custom_characters)}")
+            else:
+                print("Password must be at least 10 characters long")
+                print(f"Password (10): {custom_password_generator(10, custom_characters)}")
         elif user_input == "q":
             print("Thankyou :)")
-            time.sleep(2)
+            time.sleep(3)
             break
         else:
             print("Enter valid input")
